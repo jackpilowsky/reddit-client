e80251fe96d7e6dc7e7b513e336946bec7f3b7ba
Jack Pilowsky <jackpilowsky@gmail.com>
2018-11-19 21:14:03 -0800

    Moving the logic to the parent component

--- src/PostsPage/SortSelector.js
+++ src/PostsPage/SortSelector.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import FilledInput from '@material-ui/core/FilledInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
@@ -27,17 +22,7 @@ const endpoints = [
 ];
 
 
-class SimpleSelect extends React.Component {
-  constructor(props){
-    super(props); 
-    this.state = {
-      sortby: 'new'
-    };
-  }
-
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+class SortSelector extends React.Component {
 
   render() {
     const { classes } = this.props;
@@ -45,10 +30,10 @@ class SimpleSelect extends React.Component {
     return (
       <form className={classes.root} autoComplete="off">
         <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="age-simple">Sort By</InputLabel>
+          <InputLabel htmlFor="sortby">Sort By</InputLabel>
           <Select
-            value={this.state.sortby}
-            onChange={this.handleChange}
+            value={this.props.sortby}
+            onChange={this.props.handleChange}
             inputProps={{
               name: 'sortby',
               id: 'sortby',
@@ -68,8 +53,8 @@ class SimpleSelect extends React.Component {
   }
 }
 
-SimpleSelect.propTypes = {
+SortSelector.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleSelect);
+export default withStyles(styles)(SortSelector);
--- src/PostsPage/index.js
+++ src/PostsPage/index.js
@@ -3,11 +3,21 @@ import Grid from '@material-ui/core/Grid';
 import SortSelector from './SortSelector';
 
 class PostsPage extends Component{
+  constructor(props){
+    super(props); 
+    this.state = {
+      sortby: 'new'
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render(){
     return(
       <Grid container>
         <Grid item xs={12}>
-          <SortSelector />
+          <SortSelector onChange={this.handleChange} sortby={this.state.sortby} />
         </Grid>
       </Grid>      
     )
